74dac81748a6318128d96309365b1913f3791789
watariRyo <72895473+watariRyo@users.noreply.github.com>
2023-02-15 22:33:24 +0900

    added refresh token

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,8 +1,10 @@
 import { SPOTIFY_BASE_URL } from "$env/static/private";
+import { redirect } from "@sveltejs/kit";
 import type { LayoutServerLoad } from "./$types";
 
-export const load: LayoutServerLoad = async({ cookies }) => {
+export const load: LayoutServerLoad = async({ cookies, url, fetch }) => {
     const accessToken = cookies.get('access_token');
+    const refreshToken = cookies.get('refresh_token');
     if (!accessToken) {
         return {
             user: null
@@ -18,6 +20,11 @@ export const load: LayoutServerLoad = async({ cookies }) => {
         const profile: SpotifyApi.CurrentUsersProfileResponse = await profileRes.json()
         return {
             user: profile
+        }
+    } if (profileRes.status === 401 && refreshToken) {
+        const refrfeshRes = await fetch('/api/auth/refresh')
+        if (refrfeshRes.ok) {
+            throw redirect(307, url.pathname)
         }
     } else {
         return {
