58a2071c1c9dc902a1d9d0b92638b78a6cef8b00
watariRyo <72895473+watariRyo@users.noreply.github.com>
2023-02-16 21:08:35 +0900

    add logout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
+	import { invalidateAll } from "$app/navigation";
     import "modern-normalize/modern-normalize.css"
+	import { LogoutButton } from "$components";
     import '../styles/main.scss';
 	import type { LayoutData } from "./$types";
 
     export let data: LayoutData
+
+    $: user = data.user;
 </script> 
 
-{data.user?.display_name}
+{#if user}
+    <p>Hello {user.display_name}</p>
+    <LogoutButton />
+{/if}
 
 <slot />
